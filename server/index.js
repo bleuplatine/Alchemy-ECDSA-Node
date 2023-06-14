d1aa3133833f3f846df698502c9e1bc51e1e1a39
Sébastien <bleuplatine@gmail.com>
2023-06-14 17:56:40 +0200

    Ready to begin

--- server/index.js
+++ server/index.js
@@ -7,9 +7,10 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  // Public Keys
+  "028bc0838d35c1b9b8e70dd1172aedef527eff64261b2cc630664823b1c838243a": 100,
+  "0311171ee52074c04fd8faea6a0c8dab53b5a2f76494f1fe3a006d9bd07fb899e6": 50,
+  "02d1d8d4f561a016441a5c7ea209adb99d2c3063588df6adeb81d46bf0faa24f62": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,8 +20,11 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  // TODO: get a signature from the client-side application
+  // recover the public address from the signature
+  
   const { sender, recipient, amount } = req.body;
-
+  console.log('sender', sender)
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
